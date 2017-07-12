24f4a829124f80687ed854b22be9e4960c4ee978
peretzl <lbp43@drexel.edu>
2017-07-12 11:02:00 -0400

    palindrone challenge

---
+++ palindrone.js
@@ -0,0 +1,23 @@
+function palindrome(str){
+  var reversedStr = "";
+  var lowerStr = str.toLowerCase();
+  
+  for(var length = lowerStr.length - 1; length >= 0; length--){  	
+  		reversedStr += lowerStr[length];  
+  }
+
+  if(reversedStr === lowerStr){
+    return true;
+  }
+
+  return false;
+}
+
+
+function palindrome(str) {
+  for (var i = 0; i < str.length; i++)
+    if (str[i] !== str[str.length - i - 1]){
+      return false;
+    }
+  return true;
+}
